867efec296de6e0e62cf9e7852e7fffe1b7a95af
Kyo <kyo@ubuntu.ubuntu-domain>
2015-10-16 16:10:01 +0800

    完成部分sec.c

--- sec/sec.c
+++ sec/sec.c
@@ -6,6 +6,8 @@
  ************************************************************************/
 
 #include "./sec.h"
+#include "../pssme/pssme.h"
+#include "../data/data.h"
 #define INIT(m) memset(&m,0,sizeof(m))
 
 result sec_signed_wsa(struct sec_db* sdb,
@@ -16,7 +18,26 @@ result sec_signed_wsa(struct sec_db* sdb,
                 string* signed_wsa){
     result ret = SUCCESS;
     struct certificate_chain chain;
-    int *permission_indices = (int *)malloc(sizeof(int)*permissions->len);
-    memset(permission_indices, 0, sizeof(permissions->len));
     INIT(chain);
+    string *permission_indices = string_malloc(permissions->len);
+    cmh cmh;    
+    two_d_location *two_dl = get_current_location();
+
+    ret = pssme_cryptomaterial_handle(sdb, permissions, two_dl, permission_indices, &cmh, &chain);
+
+    if(ret == SUCCESS){
+        tobesigned_wsa tbs_wsa;
+        INIT(tbs_wsa);
+
+        tbs_wsa.permission_indices.len = permission_indices->len;
+        memcpy(tbs_wsa.permission_indices.buf, permission_indices->buf, permission_indices->len);
+
+        
+        
+        tbs_wsa.data.len = data->len;
+        memcpy(tbs_wsa.data.buf, data->buf, data->len);
+
+        tbs_wsa.expiry_time = life_time;
+    }
+    return ret;
 }
--- sec/sec.h
+++ sec/sec.h
@@ -3,7 +3,6 @@
 #include"../pssme/pssme_db.h"
 #include"../cme/cme_db.h"
 #include"../cme/cme.h"
-#include"../pssme/pssme.h"
 
 struct sec_db{
     struct cme_db cme_db;
@@ -38,7 +37,7 @@ enum sign_with_fast_verification{
  * @signed_data:填写好了数据，编码,输入到signed_data.
  * @len_of_cert_chain:该证书连的长度。
  */
-result sec_signed_data(struct sec_db* sdb,
+result sec_signed_data(struct cme_db* cdb,
                 content_type type,string* data,
                 string* exter_data,psid psid,
                 string* ssp,
@@ -63,7 +62,7 @@ result sec_signed_data(struct sec_db* sdb,
 /*
  * 加密数据，参数参考协议
  */
-result sec_encrypted_data(struct sec_db* sdb,
+result sec_encrypted_data(struct cme_db* cdb,
                 content_type type,
                 string* data,
                 struct certificate_chain* certs,
@@ -74,7 +73,7 @@ result sec_encrypted_data(struct sec_db* sdb,
                 struct certificate_chain* failed_certs);
 
 
-result sec_secure_data_content_extration(struct sec_db* sdb,cmh cmh,
+result sec_secure_data_content_extration(struct cme_db* cdb,cmh cmh,
                 
                 content_type *type,
                 content_type *inner_type,
@@ -94,7 +93,7 @@ result sec_secure_data_content_extration(struct sec_db* sdb,cmh cmh,
  * 验证数据的签名，
  * 参数参考协议.
  */
-result sec_signed_data_verification(struct sec_db* sdb,
+result sec_signed_data_verification(struct cme_db* cdb,
                 cme_lsis lsis,
                 psid psid,
                 content_type type,
@@ -117,12 +116,12 @@ result sec_signed_data_verification(struct sec_db* sdb,
                 three_d_location* generation_location,
                 time64 overdue_crl_tolerance);
 /**
- * crl的签名验证  时间单位秒
+ * crl的签名验证
  */
-result sec_crl_verification(struct sec_db* sdb,string* crl,time32 overdue_crl_tolerance,
+result sec_crl_verification(struct cme_db* cdb,string* crl,time64 overdue_crl_tolerance,
                         
-                time32* last_crl_time,
-                time32* next_crl_time,
+                time64* last_crl_time,
+                time64* next_crl_time,
                 certificate* cert);
 
 enum transfer_type{
@@ -133,7 +132,7 @@ enum transfer_type{
  * 生成一个证书请求报,时间单位是秒
  * 参数请看协议
  */
-result sec_get_certificate_request(struct sec_db* sdb,signer_identifier_type type,
+result sec_get_certificate_request(struct cme_db* cdb,signer_identifier_type type,
                 cmh cmh,
                 holder_type cert_type,
                 enum transfer_type transfer_type,
@@ -152,7 +151,7 @@ result sec_get_certificate_request(struct sec_db* sdb,signer_identifier_type typ
                 string* cert_request,
                 certid10* request_hash);
 
-result sec_certificate_response_processing(struct sec_db* sdb,
+result sec_certficate_response_processing(struct cme_db* cdb,
                 cmh cmh,
                 string* data,
                 
@@ -161,7 +160,7 @@ result sec_certificate_response_processing(struct sec_db* sdb,
                 certificate_request_error_code* error,
                 certificate* certificate,
                 string* rec_value,
-                bool* ack_request
+                bool ack_request
                 );
 
 result sec_signed_wsa(struct sec_db* sdb,
@@ -219,6 +218,7 @@ result sec_certificate_response_verification(struct sec_db* sdb,
                 tobe_encrypted_certificate_response* cert_resp);
 
 /***************这后面的函数都是certificate的一些帮助接口，方便获取证书的相关信息******/
+two_d_location *get_current_location();
 
 
 #endif 
