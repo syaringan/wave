cc9fa084b87990793a2e1c9d687ea6e897ef26fe
283178170@qq.com <ljh@ljh.(none)>
2015-10-19 18:59:51 +0800

    完成了cmp，等待测试检验

--- cmp/cmp.h
+++ cmp/cmp.h
@@ -1,10 +1,11 @@
 #ifndef CMP_H
 #define CMP_H
 #include "../utils/common.h"
+#include "../data/data.h"
 void cmp_do_certificate_applycation();
 void cmp_do_recieve_data();
 u32 cmp_init();
 void cmp_end();
 void cmp_run();
-void cmp_do_crl_req(crl_serial crl_serial);
+void cmp_do_crl_req(u32  crl_serial,hashedid8* issuer);
 #endif
